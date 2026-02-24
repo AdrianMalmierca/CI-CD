3dce589859670441318eb194aff0f1e905d8a664
AdeulianSuke <>
2026-02-24 15:18:31 +0100

    added routes

--- server.js
+++ server.js
@@ -1,2 +1,13 @@
-const apiRoutes = require("./routes/api");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const apiRoutes = require("./routes/api.js");
 app.use("/", apiRoutes);
+
+// Solo iniciar servidor si no estamos en test
+if (process.env.NODE_ENV !== "test") {
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+
+module.exports = app; // exportar para tests
